ab2369f53206f454208194beb1e8296130e7460d
Walt Grata <wegrata@ubuntu.(none)>
2011-01-27 09:50:08 -0500

    LR/ 	build/ 	util.pyc

--- replicate.py
+++ replicate.py
@@ -8,8 +8,8 @@ def replacate_across_servers(main_url, replication_servers, database_name):
         create_database(replication['url'],replication['db_name'])
         couch.replicate(database_name,replication['url']+ replication['db_name'])
 if __name__ == '__main__':
-    main_url = 'http://192.168.42.128:5984/'
-    server_database = 'knownservers'
+    main_url = 'http://localhost/'
+    server_database = 'knowservers'
     database_name = 'learningregistry'
     replication_servers =  []
     db = get_database(main_url,server_database)
