47e1ad50cb24b8efbbf0a2b7b377d336337508cc
Walt Grata <wegrata@ubuntu.(none)>
2011-01-28 10:53:45 -0500

    changed dc sync to use localhost

--- sync_dc.py
+++ sync_dc.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
-import couchdb, sys
+import couchdb,
 from oaipmh.client import Client
 from oaipmh.common import Identify, Metadata, Header
 from oaipmh.metadata import MetadataRegistry, oai_dc_reader , MetadataReader
 from util import *
-def format(doc):
+def format(doc):s
     if not ''.join(doc.getField('identifier')).startswith('fedora'):
         value = {'title':''.join(doc.getField('title')),'identifier':''.join(doc.getField('identifier')), 'location': ''}
         return value
 if __name__ == '__main__':
-    main_url = 'http://wegrata:g15t15@wegrata.couchone.com'    
+    main_url = 'http://localhost:5984'    
     oai_url  = 'http://localhost:8080/fedora/oai'
     database_name = 'learningregistry2'
     index_documents(oai_url,main_url,database_name,oai_dc_reader,'oai_dc', format)
