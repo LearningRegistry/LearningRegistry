f58f8c0dab32464b25f23c75242f7cd96b28ef17
Walt Grata <wegrata@ubuntu.(none)>
2011-02-02 12:19:11 -0500

    update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 apt-get install python-pip
-apt-get install git
+apt-get install lighttpd
 pip install pylons
 pip install flup
 pip install --upgrade couchdb 
-git clone git://github.com/LearningRegistry/LearningRegistry.git
 cp LearningRegistry/lighttpd.conf /etc/lighttpd/lighttpd.conf
