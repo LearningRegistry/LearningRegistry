1294380c971fb8b77357d87cbef76dc29c08044d
Walt Grata <wegrata@ubuntu.(none)>
2011-02-02 12:23:43 -0500

    fixed a file path in install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ apt-get install lighttpd
 pip install pylons
 pip install flup
 pip install --upgrade couchdb 
-cp LearningRegistry/lighttpd.conf /etc/lighttpd/lighttpd.conf
+cp lighttpd.conf /etc/lighttpd/lighttpd.conf
