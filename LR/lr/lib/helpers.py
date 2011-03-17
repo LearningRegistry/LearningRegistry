87cf923b7620028b8e2cd096f9ce71d300bb6473
John Poyau <john.poyau@lmco.com>
2011-03-17 13:52:47 -0400

    Add method that loads a module from a file

--- LR/lr/lib/helpers.py
+++ LR/lr/lib/helpers.py
@@ -5,3 +5,22 @@ available to Controllers. This module is available to templates as 'h'.
 """
 # Import helpers as desired, or define your own, ie:
 #from webhelpers.html.tags import checkbox, password
+def importModuleFromFile(fullpath):
+    """Loads  and returns module defined by the file path. Returns None if file could 
+    not be loaded"""
+    import os
+    import sys
+    import logging
+    log = logging.getLogger(__name__)
+    
+    sys.path.append(os.path.dirname(fullpath))
+    
+    module = None
+    try:
+        module = __import__(os.path.splitext(os.path.basename(fullpath))[0])
+ 
+    except Exception as ex:
+        log.exception("Failed to load module:\n"+ex)
+    finally:
+        del sys.path[-1]
+        return module
