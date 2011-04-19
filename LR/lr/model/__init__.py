2756f4aa8dfac46537343859da010a7ab61b8f4a
John Poyau <john.poyau@lmco.com>
2011-04-19 15:56:06 -0400

    Read the node configuration only and directly from the database.

--- LR/lr/model/__init__.py
+++ LR/lr/model/__init__.py
@@ -23,15 +23,64 @@ from network_policy import NetworkPolicyModel
 from  node_config import LRNodeModel
 from base_model import defaultCouchServer
 
-LR_NODE_CONFIG = appConfig['init.LRNode.config']
+#LR_NODE_CONFIG = appConfig['init.LRNode.config']
 
-lrconfig = h.importModuleFromFile(LR_NODE_CONFIG)
+#lrconfig = h.importModuleFromFile(LR_NODE_CONFIG)
 
 _ERROR = 'error'
 _OK = "OK"
-LRNode = LRNodeModel(lrconfig)
-LRNode.save()
+#LRNode = LRNodeModel(lrconfig)
+#LRNode.save()
 
+def _getNodeDocument(docModel, docType, numType=None, isRequired=True):
+    
+    docs = docModel.getAll()
+    docModels = filter(lambda row :'doc_type' in row and row['doc_type']==docType, docs)
+    numModelDocs = len(docModels)
+    
+    if numType is not None and isRequired and numModelDocs != numType:
+            raise(Exception("Error {0} of type '{1}' is required in database '{2}'".
+                    format(numType, docType, docModel._defaultDB.name)))
+    elif numModelDocs == 1:
+        model= docModel(docModels[0])
+        model.validate()
+        return model.specData
+    elif numType ==1 and numModelDocs == 0:
+        return {}
+    else:
+        results = []
+        for d in docModels: 
+            model = docModel(d)
+            model.validate()
+            results.append(model.specData)
+        return results
+
+
+def getNodeConfig():
+    """Function that get the node configuration from couchDB"""
+    #get community desciption.
+    docs = CommunityModel.getAll()
+    nodeConfig = {}
+    
+    nodeConfig['community_description']=_getNodeDocument(CommunityModel,
+                                                                                'community_description', 1)
+    nodeConfig['network_description'] = _getNodeDocument(NetworkModel, 
+                                                                                'network_description', 1)
+    nodeConfig['network_policy_description'] = _getNodeDocument(NetworkPolicyModel,
+                                                                                'policy_description', 1)
+    nodeConfig['node_description'] = _getNodeDocument(NodeModel, 
+                                                                                "node_description", 1)
+    nodeConfig['node_filter_description'] = _getNodeDocument(NodeFilterModel,
+                                                                                "filter_description", 1, False)
+    nodeConfig['node_services'] =_getNodeDocument(NodeServiceModel, 
+                                                                                    "service_description",None)
+    nodeConfig['connection_description'] = _getNodeDocument(NodeConnectivityModel,
+                                                                                "connection_description", None)
+    return nodeConfig
+    
+LRNode = LRNodeModel(getNodeConfig())
+#LRNode.save()
+    
 def isResourceDataFilteredOut(resourceData):
     
     if (LRNode.filterDescription is None or 
