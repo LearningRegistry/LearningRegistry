b3fc42c97fae826b9fba33646d1c0a17bbbb70d2
walt grata <walt.grata.ctr@adlnet.gov>
2011-04-28 14:08:44 -0400

    changed production.ini to reduce log level

--- LR/tests/test_distribute.sh
+++ LR/tests/test_distribute.sh
@@ -1,1 +1,1 @@
-curl -d "resource_documents_database=resource_data" http://lrdev3.learningregistry.org/distribute > dis.htm && firefox dis.htm &
+curl -d "resource_documents_database=resource_data" http://lrdev4.learningregistry.org/distribute > dis.htm && firefox dis.htm &
