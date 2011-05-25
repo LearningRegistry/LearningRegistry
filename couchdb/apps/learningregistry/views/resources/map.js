1cbb9abf791a2e260e0fca9f15646c9fc32465d8
John Poyau <john.poyau@lmco.com>
2011-05-25 12:52:51 -0400

    Update the obtain service to use the full resource_data document.

--- couchdb/apps/learningregistry/views/resources/map.js
+++ couchdb/apps/learningregistry/views/resources/map.js
@@ -1,6 +1,5 @@
 function(doc) {
-  if (doc.doc_type == 'resource_data_timestamp'){
-      emit(doc.resource_doc_id, {_id: doc.resource_doc_id, 
-                                    'timestamp':{'node_timestamp':doc.node_timestamp}});
+  if (doc.doc_type == 'resource_data'){
+      emit(doc.doc_ID, null);
   }  
 }
