7adcd23388a597240f89a3dc321622527892c63c
John Poyau <john.poyau@lmco.com>
2011-05-26 18:23:43 -0400

    Update to use the spec version 0.21.0 data models.

--- config/lrnodetemplate.py
+++ config/lrnodetemplate.py
@@ -2,13 +2,13 @@ import time
 
 node_description ={
     "doc_type":		"node_description",
-    "doc_version":		"0.10.0",
+    "doc_version":		"0.21.0",
     "doc_scope":		"node",	
     "active":			True,
     "node_id":		"<nodeid>",	
     "node_name": 		"<user provided nodename>",
     "node_description":	"<user provided nodedesciption>",
-    "node_admin_url":	"<user provided nodeadmin>",	
+    "node_admin_identity":	"<user provided nodeadmin>",	
     "network_id":		"Learning Registry Open, Public Network",
     "community_id":		"Learning Registry Open, Public Community",	
     "gateway_node":	False,
@@ -30,14 +30,14 @@ connection_description =   {
     
 network_description =  {
     "doc_type":		"network_description",
-    "doc_version":		"0.10.0",
+    "doc_version":		"0.20.0",
     "doc_scope":		"network",
     "active":			True,	
     "network_id":		"Learning Registry Open, Public Network",
     "network_name":	"Learning Registry Open, Public Network",
     "network_description":	"Learning Registry Open, Public Network",
     "community_id":		"Learning Registry Open, Public Community",	
-    "network_admin_url":	"network.admin@learningregistry.org"
+    "network_admin_identity":	"network.admin@learningregistry.org"
 }
 
 network_policy_description ={
@@ -46,26 +46,26 @@ network_policy_description ={
     "doc_scope":		"network",
     "active":			True,
     "network_id":		"Learning Registry Open, Public Network",
-    "policy_id":		"Learning Registry Open, Public Network Policy",	
+    "policy_id":		"Learning Registry Open, Public Network Policy",
      "policy_version":	"0.10.0",
      "TTL":			400
 }
 
 community_description ={
     "doc_type":		"community_description",
-    "doc_version":		"0.10.0",
+    "doc_version":		"0.20.0",
     "doc_scope":		"community",
     "active":			True,
     "community_id":		"Learning Registry Open, Public Community",	
     "community_name":	"Learning Registry Open, Public Community",	
     "community_description":"Learning Registry Open, Public Community",
-    "community_admin_url":"community.admin@learningregistry.org",
+    "community_admin_identity":"community.admin@learningregistry.org",
     "social_community":	True
 }
 
 service_description= {
         "doc_type": "service_description",
-        "doc_version": "0.10.0",
+        "doc_version": "0.20.0",
         "doc_scope": "node", 
         "active": True,
         "service_id": "<service_id>",
@@ -74,6 +74,9 @@ service_description= {
         "service_description": "<service description>", 
         "service_version": "0.1", 
         "service_endpoint": "string", 
-        "service_auth": "public",
+        "service_auth": 
+                    {"service_authz":"none",
+                      "service_key":False,
+                     "service_https":False},
         "service_data":{}
 }
