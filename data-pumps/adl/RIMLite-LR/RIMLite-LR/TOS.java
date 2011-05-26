2982e4bb907b51bc3e2b9cff802ce902fb7707bb
Walt Grata <wegrata@ubuntu.(none)>
2011-05-26 08:49:25 -0700

    adding adl datapumps for 3dr and ADL Registry

---
+++ data-pumps/adl/RIMLite-LR/RIMLite-LR/TOS.java
@@ -0,0 +1,21 @@
+public class TOS
+{
+	private String submission_TOS;
+	private String submission_attribution;
+	public String get_submission_TOS()
+	{
+		return submission_TOS;
+	}
+	public void set_submission_TOS(String value)
+	{
+		submission_TOS = value;
+	}
+	public String get_submission_attribution()
+	{
+		return submission_attribution;
+	}
+	public void set_submission_attribution(String value)
+	{
+		submission_attribution = value;
+	}	
+}
