368f7cda8cf44f67d5118bf8632c984221556059
Jim Klo <jim.klo@sri.com>
2011-06-12 01:21:22 -0700

    fixed unicode issues with data and import
    Signed-off-by: Jim Klo <jim.klo@sri.com>

--- utils/import.py
+++ utils/import.py
@@ -1,10 +1,24 @@
-import urllib2, os, libxml2, json
+import urllib2, os, libxml2, json, ConfigParser, argparse
 
 rootPath = 'C:\\Documents and Settings\\admin\\Desktop\\json\\20110228_eun_MDlre4_LR_0_10_0_100'
 publish_url = 'http://12.109.40.15/publish'
 docs = []
 write_to_file = False
 output_file_path = 'C:\\Documents and Settings\\admin\\Desktop\\json\\20110228_eun_MDlre4_LR_0_10_0_180000'
+
+_parser = argparse.ArgumentParser(description='Convert RAW EUN XML Metadata to LR Envelopes')
+_parser.add_argument('--config', help='INI configuration file. See testconfig.ini.sample', default='testconfig.ini')
+args = _parser.parse_args()
+
+_config = ConfigParser.ConfigParser()
+_config.read(args.config)
+
+rootPath = _config.get("import", "root_path")
+output_file_path = _config.get("import", "out_path")
+write_to_file = _config.get("import", "write_to_file").lower() in ["true"]
+
+width  = 0
+
 if os.path.exists(os.path.join(rootPath,'error.html')):
   #delete old error log file if it exists
   os.remove(os.path.join(rootPath,'error.html'))
@@ -13,10 +27,18 @@ def process_docs(count):
   if(write_to_file):
     if not os.path.exists(output_file_path):
       os.mkdir(output_file_path)
+    fmt = '2011-02-28Metadata{0:0'+str(width)+'d}.json'
     for i in range(0,len(docs)):
-      if os.path.exists(os.path.join(output_file_path,'2011-02-28Metadata'+str(i + count)+'.json')):
-        with open(os.path.join(output_file_path,'2011-02-28Metadata'+str(i + count)+'.json'),'w') as f:
-	      f.write(json.dumps(docs[i]))
+      if os.path.exists(output_file_path):
+          
+          outFile = os.path.join(output_file_path,fmt.format(i + count))
+          
+          if os.path.exists(outFile) == True:
+              os.remove(outFile)
+                                     
+          with open(outFile,'w') as f:
+              f.write(json.dumps(docs[i], sort_keys=True, indent=4))
+              print outFile
   else:
     #convert to JSON
     data =json.dumps({'documents':docs})
@@ -28,45 +50,59 @@ def process_docs(count):
       #if something bad happened dump the error to an html file so it can be displayed in a browser
       with open(os.path.join(rootPath,'error.html'),'w') as f:
         for line in err.readlines():
-          f.write(line)
+          f.write(line),
       output = os.path.join(rootPath,'error.html')
       print 'see file ' + output
-doc_count = 0
-for childDir in os.listdir(rootPath):
-  childPath = os.path.join(rootPath,childDir)
-  for file in os.listdir(childPath):  
-    #use xpath to pull relevant fields out of the xml doc
-    doc = libxml2.parseFile(os.path.join(childPath , file))
-    context = doc.xpathNewContext()
-    context.xpathRegisterNs('oai','http://www.openarchives.org/OAI/2.0/')
-    context.xpathRegisterNs('ims','http://www.imsglobal.org/xsd/imslorsltitm_v1p0')
-    context.xpathRegisterNs('lom','http://ltsc.ieee.org/xsd/LOM')
-    #we only need to content of the xml nodes	
-    location = context.xpathEval("//ims:expression/ims:manifestation/ims:item/ims:location/ims:uri/text()")[0].getContent()
-    doc.freeDoc()  
-    context.xpathFreeContext()
-    #read the raw XML as a UTF-8 string
-    with open(os.path.join(childPath,file),'r+') as f:
-      read_data = json.dumps(f.read())
-    doc = {
-           "doc_type":        "resource_data",
-           "doc_version":        "0.10.0",
-           "active":        True,
-           "resource_data_type":    "metadata",
-           "submitter_type":    "anonymous",
-           "update_timestamp":    "01/12/2011",
-           "create_timestamp":    "01/12/2011",		   
-           "submitter":        "test data set submitter",
-           "submission_TOS":    "http://www.learningregistry.org/tos-v1-0.html",
-           "resource_locator":    location,
-           "keys":        ["EUN", "LOM", "test data"],
-           "payload_placement":    "inline",
-           "payload_schema":    ["LODE, LOM"],
-           "resource_data": read_data,
-		   'publishing_node': 'nsdl'
-          }  
-    docs.append(doc)
-    if len(docs) > 1000:
-      process_docs(doc_count)
-      doc_count = doc_count + len(docs)
-      docs = []
+      
+def parse_raw_docs():
+    global docs, width
+    width = len(str(len(os.listdir(rootPath))-1))      
+    doc_count = 0
+    for fname in os.listdir(rootPath):
+        rootFile = os.path.join(rootPath,fname) 
+        #  for file in os.listdir(childPath): use xpath to pull relevant fields out of the xml doc
+        xmldoc = libxml2.parseFile(rootFile)
+        context = xmldoc.xpathNewContext()
+        context.xpathRegisterNs('oai','http://www.openarchives.org/OAI/2.0/')
+        context.xpathRegisterNs('ims','http://www.imsglobal.org/xsd/imslorsltitm_v1p0')
+        context.xpathRegisterNs('lom','http://ltsc.ieee.org/xsd/LOM')
+        #we only need to content of the xml nodes    
+        location = context.xpathEval("//ims:expression/ims:manifestation/ims:item/ims:location/ims:uri/text()")[0].getContent()
+        xmldoc.freeDoc()  
+        context.xpathFreeContext()
+        #read the raw XML as a UTF-8 string
+        with open(rootFile,'r+') as f:
+          read_data = f.read()
+          
+        doc = {
+               "doc_type":              "resource_data",
+               "doc_version":           "0.21.0",
+               "active":                True,
+               "resource_data_type":    "metadata",
+               "resource_locator":      location,
+               "keys":                  ["EUN", "LOM", "lr-test-data" ],
+               "payload_placement":     "inline",
+               "payload_schema":        ["LODE", "LOM"],
+               "resource_data":         read_data,
+               "identity": {
+                            "submitter_type":   "agent",
+                            "submitter":        "Learning Registry",
+                            "curator":          "European School Net",
+                            "owner":            "European School Net"
+        #                        'signer':''
+                },
+               "TOS": {
+                       "submission_TOS":        "http://www.learningregistry.org/tos/cc0/v0-5/"
+                },
+              }  
+        docs.append(doc)
+        if len(docs) > 1000:
+            yield (doc_count, docs)
+            doc_count = doc_count + len(docs)
+            docs = []
+    if len(docs) > 0:
+        yield (doc_count, docs)
+        
+
+for (doc_count, batch) in parse_raw_docs():
+    process_docs(doc_count)
--- utils/upload.py
+++ utils/upload.py
@@ -9,14 +9,19 @@ parser = argparse.ArgumentParser(description='Sign files and upload to Learning 
 parser.add_argument('--key', help='PGP Private Key ID')
 parser.add_argument('--key-location', help='Location the PGP Public Key can be downloaded from')
 parser.add_argument('--passphrase', help='Passphrase for PGP Private Key', default=None)
+parser.add_argument('--gpgbin', help='Path to GPG binary', default="gpg")
+parser.add_argument('--publish-url', help='Learning Registry Node publish url', default=None)
 args = parser.parse_args()
 if args.key is not None and args.key_location is not None:
-    signer = Sign_0_21(privateKeyID=args.key ,publicKeyLocations=[args.key_location], passphrase=args.passphrase )
+    signer = Sign_0_21(privateKeyID=args.key ,publicKeyLocations=[args.key_location], passphrase=args.passphrase, gpgbin=args.gpgbin)
 _config = ConfigParser.ConfigParser()
 _config.read('testconfig.ini')
 root_path = _config.get("upload", "root_path")
 publish_url = _config.get("upload", "publish_url")
 publish_urls = ['http://lrdev1.learningregistry.org/publish','http://lrdev2.learningregistry.org/publish','http://lrdev3.learningregistry.org/publish']
+
+if args.publish_url != None:
+    publish_url = args.publish_url
 
 def upload_files(docs):
   try:
@@ -32,12 +37,15 @@ def upload_files(docs):
     print 'error'
 def process_files():
     documents=[]
-    for file in os.listdir(root_path):
-        file_path = os.path.join(root_path,file)
+    numfiles = len(os.listdir(root_path))
+    for fname in os.listdir(root_path):
+        file_path = os.path.join(root_path,fname)
         with codecs.open(file_path,'r+', 'utf-8-sig') as f:
             data = json.load(f)        
-        if signer is not None:            
+        if signer is not None: 
+            print '{1}: signing {0}'.format(fname, numfiles)           
             data = signer.sign(data)
+            numfiles -= 1
         documents.append(data)
         if len(documents) >= 10:
             upload_files(documents)
