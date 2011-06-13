668932d9163758dd2c16c37b1c3629a15bf0327f
Jim Klo <jim.klo@sri.com>
2011-06-13 11:15:56 -0700

    added a few more command line options
    for details see upload.py --help
    
    --data-dir: directory of source LR Data Envelopes
    --lr-test-data: True or False, adds or removes lr-test-data key from each envelope when publishing.
    --config: use an alternate configuration INI file if desired
    Signed-off-by: Jim Klo <jim.klo@sri.com>

--- utils/upload.py
+++ utils/upload.py
@@ -10,18 +10,26 @@ parser.add_argument('--key', help='PGP Private Key ID')
 parser.add_argument('--key-location', help='Location the PGP Public Key can be downloaded from')
 parser.add_argument('--passphrase', help='Passphrase for PGP Private Key', default=None)
 parser.add_argument('--gpgbin', help='Path to GPG binary', default="gpg")
-parser.add_argument('--publish-url', help='Learning Registry Node publish url', default=None)
+parser.add_argument('--publish-url', help='Learning Registry Node publish url. Overrides publish_url in [upload] from configuration file', default=None)
+parser.add_argument('--data-dir', help='Directory of source LR Data Envelopes. Overrides root_path in [upload] from configuration file"', default=None)
+parser.add_argument('--lr-test-data', help='Publish as lr test data, default is True', default="True")
+parser.add_argument('--config', help='Configuration file, default is "testconfig.ini"', default="testconfig.ini")
+
 args = parser.parse_args()
 if args.key is not None and args.key_location is not None:
     signer = Sign_0_21(privateKeyID=args.key ,publicKeyLocations=[args.key_location], passphrase=args.passphrase, gpgbin=args.gpgbin)
 _config = ConfigParser.ConfigParser()
-_config.read('testconfig.ini')
+_config.read(args.config)
 root_path = _config.get("upload", "root_path")
 publish_url = _config.get("upload", "publish_url")
 publish_urls = ['http://lrdev1.learningregistry.org/publish','http://lrdev2.learningregistry.org/publish','http://lrdev3.learningregistry.org/publish']
+lr_test_data = args.lr_test_data.lower() in ["true", "t", "yes" "1"]
 
 if args.publish_url != None:
     publish_url = args.publish_url
+
+if args.data_dir != None:
+    root_path = args.data_dir
 
 def upload_files(docs):
   try:
@@ -35,6 +43,23 @@ def upload_files(docs):
     with open('error.html','a') as out:
       out.write(er.read())
     print 'error'
+    
+def set_test_key(data, remove=True):
+    rmcount = 0
+    for item in data["keys"]:
+        if item == "lr-test-data":
+            rmcount += 1
+    if remove:
+        while rmcount > 0:
+            try:
+                data["keys"].remove("lr-test-data")
+            except:
+                pass
+            rmcount += -1
+    elif not remove and rmcount == 0:
+        data["keys"].append("lr-test-data")
+    return data
+        
 def process_files():
     documents=[]
     numfiles = len(os.listdir(root_path))
@@ -42,6 +67,12 @@ def process_files():
         file_path = os.path.join(root_path,fname)
         with codecs.open(file_path,'r+', 'utf-8-sig') as f:
             data = json.load(f)        
+            
+        if not lr_test_data and data.has_key("keys"):
+            data = set_test_key(data)
+        else:
+            data = set_test_key(data, remove=False)
+            
         if signer is not None: 
             print '{1}: signing {0}'.format(fname, numfiles)           
             data = signer.sign(data)
