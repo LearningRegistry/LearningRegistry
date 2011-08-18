cea328a83b96d4ec13550203dc91096d17a6ede2
Walt Grata <wegrata@gmail.com>
2011-08-18 10:54:20 -0400

    Added flow control consistent with Jim's

--- LR/lr/controllers/obtain.py
+++ LR/lr/controllers/obtain.py
@@ -13,36 +13,50 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 import logging 
 import json 
-import couchdb
 from lr.model.base_model import appConfig
 import lr.lib.helpers as h
+import lr.lib.resumption_token as rt
 from pylons import request, response, session, tmpl_context as c, url
 from pylons.controllers.util import abort, redirect
 from lr.lib.base import BaseController, render
 
 log = logging.getLogger(__name__)  
 trues = ['T','t','True','true']
+LIMIT = 100
 class ObtainController(BaseController):
     """REST Controller styled on the Atom Publishing Protocol"""
     # To properly map this controller, ensure your config/routing.py
     # file has a resource setup:
     #     map.resource('obtain', 'obtain')
-    def get_view(self,view_name = '_design/learningregistry/_view/resources',keys=[], include_docs = False):                
+    def get_view(self,view_name = '_design/learningregistry/_view/resources',keys=[], include_docs = False,resumption_token=None):                
+        log.debug(resumption_token)
         db_url = '/'.join([appConfig['couchdb.url'],appConfig['couchdb.db.resourcedata']])
+        args = {}
         if len(keys) > 0:
-          view = h.getView(database_url=db_url,view_name=view_name,keys=keys,include_docs=include_docs,stale='ok')
-        else:
-          view = h.getView(database_url=db_url,view_name=view_name,include_docs=include_docs,stale='ok')
+            args['keys'] = keys
+        args['stale'] = 'ok'
+        args['limit'] = LIMIT
+        args['include_docs'] = include_docs
+        if resumption_token is not None:
+            args['startkey'] = resumption_token['startkey']
+            args['startkey_docid'] = resumption_token['startkey_docid']
+            args['skip'] = 1
+        view = h.getView(database_url=db_url,view_name=view_name,documentHandler=lambda d: h.document(d),**args)
         return view
-
     def format_data(self, full_docs, data, currentResumptionToken):
         yield '{"documents":['
         num_sent = 0
         currentID = ""
         byIDResponseChunks = None
+        count = 0
+        lastStartKey = None
+        lastId = None
         if data is not None:
             firstID = True
             for doc in data:
+                lastStartKey = doc.key
+                lastId = doc.id
+                count += 1
                 if full_docs: 
                     if doc.key != currentID:                        
                         currentID = doc.key                        
@@ -60,10 +74,13 @@ class ObtainController(BaseController):
                             yield ',\n'
                         firstID = False
                         yield json.dumps({'doc_ID': doc.key})
-    
         if full_docs and byIDResponseChunks is not None:             
             yield ']' + byIDResponseChunks[1]                        
-        yield "]}"
+        if count < LIMIT:			
+            yield "]}"
+        else:
+            token = rt.get_token("obtain",startkey=lastStartKey,endkey=None,startkey_docid=lastId)
+            yield '], "resumption_token":"%s"}' % token
     def index(self, format='html'):
         """GET /obtain: All items in the collection"""        
         data = self._parseParams()
@@ -86,13 +103,13 @@ class ObtainController(BaseController):
         if not data.has_key('by_resource_ID') and not by_doc_ID:
             by_resource_ID = True            
         if data.has_key('resumption_token'):
-            resumption_token = int(data['resumption_token'])
+            resumption_token = data['resumption_token']
         if data.has_key('callback'):
             yield "{0}(".format(data['callback'])                    
         if  by_doc_ID:
-            view = self.get_view(keys=keys, include_docs=full_docs)
+            view = self.get_view(keys=keys, include_docs=full_docs,resumption_token=resumption_token)
         elif by_resource_ID:
-            view = self.get_view('_design/learningregistry/_view/resource-location',keys, include_docs=full_docs)        
+            view = self.get_view('_design/learningregistry/_view/resource-location',keys, include_docs=full_docs,resumption_token=resumption_token)        
         for i in  self.format_data(full_docs,view, resumption_token):        
             yield i
         if(data.has_key('callback')):
@@ -142,13 +159,14 @@ class ObtainController(BaseController):
         if request.params.has_key('by_doc_ID'):
             data['by_doc_ID'] = request.params['by_doc_ID'] in trues
             data['by_resource_ID'] = False
-         
+        
         if request.params.has_key('by_resource_ID'):            
             data['by_resource_ID'] = request.params['by_resource_ID'] in trues
         if request.params.has_key('ids_only'):
             data['ids_only'] = request.params['ids_only'] in trues
         if request.params.has_key('resumption_token'):
-            data['resumption_token'] = request.params['resumption_token']
+            data['resumption_token'] = rt.parse_token('obtain',request.params['resumption_token'])
+            log.debug(data['resumption_token'])
         if request.params.has_key('callback'):
             data['callback'] = request.params['callback']
         return data        
