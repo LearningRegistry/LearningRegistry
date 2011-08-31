1d8564629145265c37d9e33912b64b0931e28147
Jim Klo <jim.klo@sri.com>
2011-08-30 18:05:38 -0700

    updating slice with flow control and streaming support
    streaming support was previously present, however it was unclear how functional it was.  made it functional using the new streaming library as well as added flow control similar to other interfaces.
    
    Signed-off-by: Jim Klo <jim.klo@sri.com>

--- LR/lr/controllers/slice.py
+++ LR/lr/controllers/slice.py
@@ -9,6 +9,7 @@ from lr.lib.base import BaseController, render
 from datetime import datetime, timedelta
 from lr.lib.oaipmherrors import *
 import types
+from lr.lib import resumption_token
 
 log = logging.getLogger(__name__)
 
@@ -19,13 +20,40 @@ ANY_TAGS = 'any_tags'
 #FULL_DOCS = 'full_docs'
 IDS_ONLY = 'ids_only'
 CALLBACK = 'callback'
+RESUMPTION_TOKEN = 'resumption_token'
+
+SLICE_SERVICE_DOC = "access:slice"
 
 class SliceController(BaseController):
     """REST Controller styled on the Atom Publishing Protocol"""
     # To properly map this controller, ensure your config/routing.py
     # file has a resource setup:
     # map.resource('slice', 'slices')
-
+    def __before__(self):
+        self.enable_flow_control = False
+        self.fc_id_limit = None
+        self.fc_doc_limit = None
+        
+        self.serviceDoc = h.getServiceDocument("access:slice")
+        if self.serviceDoc != None:
+            if 'service_id' in self.serviceDoc:
+                self.service_id = self.serviceDoc['service_id']
+                
+            if 'service_data' in self.serviceDoc:
+                serviceData = self.serviceDoc['service_data']
+                if 'flow_control' in serviceData:
+                    self.enable_flow_control = serviceData['flow_control']
+                
+                if self.enable_flow_control and 'id_limit' in serviceData:
+                    self.fc_id_limit = serviceData['id_limit']
+                elif self.enable_flow_control:
+                    self.fc_id_limit = 100
+                
+                if self.enable_flow_control and 'doc_limit' in serviceData:
+                    self.fc_doc_limit = serviceData['doc_limit']
+                elif self.enable_flow_control:
+                    self.fc_doc_limit = 100
+    
 
     def _get_params(self):
         req_params = {}
@@ -47,22 +75,34 @@ class SliceController(BaseController):
         params = {}
         param_count = 0
         
-        def _set_string_param(paramKey, setifempty=True):
+        def _set_string_param(paramKey, setifempty=True, defaultWhenEmpty=""):
             if req_params.has_key(paramKey):
                 params[paramKey] = str(req_params[paramKey])
                 return True
             elif setifempty:
-                params[paramKey] = ""
+                params[paramKey] = defaultWhenEmpty
                 return False
             else:
                 return False
                 
-        def _set_boolean_param(paramKey):
+        def _set_int_param(paramKey, setifempty=True):
             if req_params.has_key(paramKey):
-                params[paramKey] = req_params[paramKey][0].lower() == 't'
+                params[paramKey] = int(req_params[paramKey])
                 return True
-            else :
+            elif setifempty:
+                params[paramKey] = None
+                return False
+            else:
+                return False
+            
+        def _set_boolean_param(paramKey, setifempty=True):
+            if req_params.has_key(paramKey):
+                params[paramKey] = req_params[paramKey].lower() in ["t", "true", "y", "yes"]
+                return True
+            elif setifempty:
                 params[paramKey] = False
+                return False
+            else:
                 return False
         
         if _set_string_param(START_DATE) : param_count += 1
@@ -71,19 +111,64 @@ class SliceController(BaseController):
         _set_string_param(END_DATE)
         _set_boolean_param(IDS_ONLY)
         _set_string_param(CALLBACK, False)
+        _set_string_param(RESUMPTION_TOKEN, True, None)
         
+        if params[RESUMPTION_TOKEN] is not None:
+            params[RESUMPTION_TOKEN] = resumption_token.parse_token(self.service_id, params[RESUMPTION_TOKEN])
+            if len([i for i in ["offset", "keys"] if i in params[RESUMPTION_TOKEN]]) != 2:
+                msg = ": Unknown Error"
+                if "error" in params[RESUMPTION_TOKEN]:
+                    msg = ": %s" % params[RESUMPTION_TOKEN]["error"]
+                raise BadArgumentError("Bad Resumption Token%s" % msg)
+                    
         params['param_count'] = param_count
         log.debug(json.dumps(params))
         return params
 
-    def _get_view(self,view_name = '_design/learningregistry-resources/_view/docs',keys=[], include_docs = False):
+    def _get_view(self,view_name = '_design/learningregistry-slice/_view/docs',keys=[], include_docs = False, resumptionToken=None, limit=None):
         db_url = '/'.join([appConfig['couchdb.url'],appConfig['couchdb.db.resourcedata']])
-        if len(keys) > 0:
-            view = h.getView(database_url=db_url, method="POST", view_name=view_name,keys=keys,include_docs=include_docs)#,stale='ok'):
+        
+        opts = {"stale": "ok", "reduce": False }
+        
+        if include_docs:
+            opts["include_docs"] = True
+        
+        if self.enable_flow_control and resumptionToken != None:
+            if resumptionToken != None:
+                opts["skip"] = resumptionToken["offset"]
+                opts["keys"] = resumptionToken["keys"]
         else:
-            view = h.getView(database_url=db_url,view_name=view_name,include_docs=include_docs)#,stale='ok'):
-        return view
+            opts["keys"] = keys
 
+        if limit != None:
+                    opts["limit"] = limit
+        
+        if len(opts["keys"]) > 0:
+            view = h.getView(database_url=db_url, method="POST", view_name=view_name, **opts)#,stale='ok'):
+        else:
+            view = h.getView(database_url=db_url,view_name=view_name, **opts)#,stale='ok'):
+        return view
+    
+    def _get_view_total(self,view_name = '_design/learningregistry-slice/_view/docs',keys=[], resumptionToken=None):
+        db_url = '/'.join([appConfig['couchdb.url'],appConfig['couchdb.db.resourcedata']])
+        
+        opts = {"stale": "ok", "reduce": True, "group": True }
+        
+        if self.enable_flow_control and resumptionToken != None:
+            opts["keys"] = resumptionToken["keys"]
+        else:
+            opts["keys"] = keys
+            
+        if len(opts["keys"]) > 0:
+            view = h.getView(database_url=db_url, method="POST", view_name=view_name, **opts)#,stale='ok'):
+        else:
+            view = h.getView(database_url=db_url,view_name=view_name, **opts)#,stale='ok'):
+            
+        totalDocs = 0
+        for row in view:
+            if "value" in row:
+                totalDocs += row["value"]
+        return totalDocs
     
     def _get_keys(self, params):
         log.debug("gettingslicekeys")
@@ -145,32 +230,49 @@ class SliceController(BaseController):
         return dates
         
         
-    def format_data(self,keys_only,docs, keys, forceUnique):
+    def format_data(self,keys_only,docs, keys, forceUnique, current_rt=None):
         sentIDs = []
         prefix = '{"replyStart":"'+str(datetime.today())+'", "keyCount":'+str(len(keys)) +', "documents":[\n'
         num_sent = 0
         doc_count = 0
         if docs is not None:
-            for doc in docs:
+            for row in docs:
                 doc_count += 1
-                alreadySent = (doc.id in sentIDs)
+                alreadySent = (row["id"] in sentIDs)
                 if not alreadySent or not forceUnique:
-                    sentIDs.append(doc.id)
+                    sentIDs.append(row["id"])
                     if keys_only:
-                        return_data = {"doc_ID":doc.id}
+                        return_data = {"doc_ID":row["id"]}
                     else:
                         # Get the resource data and update with the node timestamp data
                         # That the view has in value['timestamp']
                         resourceData = {}
-                        resourceData = doc.doc
-                        return_data = {"doc_ID":doc.id, "resource_data_description":resourceData}
+                        resourceData = row["doc"]
+                        return_data = {"doc_ID":row["id"], "resource_data_description":resourceData}
                     yield prefix + json.dumps(return_data)
                     num_sent += 1
                     prefix = ",\n"
                 else:
                     log.debug("{0} skipping: alreadySent {1} / forceUnique {2}".format(doc_count, repr(alreadySent), forceUnique))
-                    
-        yield '\n], "resultCount":'+str(num_sent) +', "replyEnd":"'+str(datetime.today())+'"}'
+        
+        if doc_count == 0:
+            yield prefix
+        
+        maxResults = self._get_view_total(keys=keys,resumptionToken=current_rt)
+        
+        rt = " "
+        if self.enable_flow_control:
+            if current_rt != None and "offset" in current_rt and current_rt["offset"] is not None:
+                offset = current_rt["offset"]
+            else:
+                offset = 0
+                
+            if offset+doc_count < maxResults:
+                rt = ''' "resumption_token":"{0}", '''.format(resumption_token.get_offset_token(self.service_id, offset=offset+doc_count, keys=keys))
+
+        
+
+        yield '\n],'+rt+'"resultCount":'+str(maxResults) +', "replyEnd":"'+str(datetime.today())+'"}'
         
 # if __name__ == '__main__':
 # param = {START_DATE: "2011-03-10", END_DATE: "2011-05-01", IDENTITY: "NSDL 2 LR Data Pump", 'search_key': 'Arithmetic'}
@@ -183,14 +285,19 @@ class SliceController(BaseController):
         """GET /slices: All items in the collection"""
         # url('slices')
         
-        
         def getResponse(keys, params):
             if len(keys) > 0 :
-                 
+                limit = None
+                if self.enable_flow_control:
+                    if params[IDS_ONLY]:
+                        limit = self.fc_id_limit
+                    else:
+                        limit = self.fc_doc_limit 
+
                 if CALLBACK in params:
                     yield "{0}(".format(params[CALLBACK])
-                docs = self._get_view('_design/learningregistry-slice/_view/docs', keys, not params[IDS_ONLY])
-                for i in self.format_data(params[IDS_ONLY],docs,keys,True):
+                docs = self._get_view('_design/learningregistry-slice/_view/docs', keys, not params[IDS_ONLY], params[RESUMPTION_TOKEN], limit)
+                for i in self.format_data(params[IDS_ONLY],docs,keys,True,params[RESUMPTION_TOKEN]):
                     yield i
                 if CALLBACK in params:
                     yield ");"
