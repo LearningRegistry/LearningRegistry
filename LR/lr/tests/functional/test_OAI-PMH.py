4687e9bd2e778c991b2d459dc8783118f20f9dcc
Jim Klo <jim.klo@sri.com>
2011-09-13 17:06:52 -0700

    Added some missing OAI-PMH functional tests.
    Signed-off-by: Jim Klo <jim.klo@sri.com>

--- LR/lr/tests/functional/test_OAI-PMH.py
+++ LR/lr/tests/functional/test_OAI-PMH.py
@@ -12,6 +12,10 @@ import datetime
 import re
 from lr.lib.oaipmh import oaipmh
 import pprint
+from lr.lib import helpers
+import iso8601
+from iso8601.iso8601 import ParseError
+import time
 
 json_headers={'content-type': 'application/json'}
 
@@ -33,8 +37,8 @@ log = logging.getLogger(__name__)
 
 
 test_data_delete = True
-nsdl_data = { "documents" : [] }
-dc_data = { "documents" : [] }
+nsdl_data = { "documents" : [], "ids": [] }
+dc_data = { "documents" : [], "ids": [] }
 
 
 
@@ -53,6 +57,25 @@ class TestOaiPmhController(TestController):
         self.db = self.o.db
         
         view_data = self.db.view('oai-pmh-test-data/docs')
+        
+        dropData = False
+        if "oaipmh.drop_data_on_launch" in config:
+            dropData =  config["oaipmh.drop_data_on_launch"] in ["true", "True"]
+        
+        if len(view_data) > 0 and dropData:
+            for data in view_data:
+                try:
+                    del self.db[data.id+"-distributable"]
+                    log.debug("Deleting id: %s" % data.id+"-distributable")
+                except:
+                    pass
+                try:
+                    del self.db[data.id]
+                    log.debug("Deleting id: %s" % data.id)
+                except:
+                    pass
+            view_data = self.db.view('oai-pmh-test-data/docs')
+        
         if (len(view_data) == 0):
             
             if hasattr(self, "attr"):
@@ -64,23 +87,28 @@ class TestOaiPmhController(TestController):
             nsdl_data = json.load(file("lr/tests/data/nsdl_dc/data-000000000.json"))
             for doc in nsdl_data["documents"]:
                 doc["doc_ID"] = "NSDL-TEST-DATA-"+str(uuid.uuid1())
-            
-            app.post('/publish', params=json.dumps(nsdl_data), headers=json_headers)
+                app.post('/publish', params=json.dumps({"documents": [ doc ]}), headers=json_headers)
+                log.debug("Published ID: %s" % doc["doc_ID"])
+                time.sleep(1)
             
             dc_data = json.load(file("lr/tests/data/oai_dc/data-000000000.json"))
             for doc in dc_data["documents"]:
                 doc["doc_ID"] = "OAI-DC-TEST-DATA-"+str(uuid.uuid1())
-                
-            app.post('/publish', params=json.dumps(dc_data), headers=json_headers)
-            view_data = self.db.view('oai-pmh/test-data')
+                app.post('/publish', params=json.dumps({"documents": [ doc ]}), headers=json_headers)
+                log.debug("Published ID: %s" % doc["doc_ID"])
+                time.sleep(1)
+ 
+            view_data = self.db.view('oai-pmh-test-data/docs')
         
-        nsdl_data = { "documents" : [] }
-        dc_data = { "documents" : [] }
+        nsdl_data = { "documents" : [], "ids": [] }
+        dc_data = { "documents" : [], "ids": [] }
         for row in view_data:
             if re.search("^NSDL-TEST-DATA-", row.key) != None and re.search("-distributable$", row.key) == None:
                 nsdl_data["documents"].append(row.value)
+                nsdl_data["ids"].append(row.id)
             if re.search("^OAI-DC-TEST-DATA-", row.key) != None and re.search("-distributable$", row.key) == None:
                 dc_data["documents"].append(row.value)
+                dc_data["ids"].append(row.id)
         opts = {
                 "startkey":"_design/",
                 "endkey": "_design0",
@@ -101,23 +129,46 @@ class TestOaiPmhController(TestController):
     @classmethod       
     def tearDownClass(self):
         global test_data_delete
-        
+        log.info ("Tearing Down Test")
         if test_data_delete == True:
             for doc in nsdl_data["documents"]:
-                del self.db[doc["_id"]]
+                try:
+                    del self.db[doc["_id"]]
+                    log.debug("Deleted ID: %s" % doc["_id"])
+                except:
+                    pass
                 try:
                     del self.db["{0}-distributable".format(doc["_id"])]
+                    log.debug("Deleted ID: %s" % "{0}-distributable".format(doc["_id"]))
                 except:
                     pass
             for doc in dc_data["documents"]:
-                del self.db[doc["_id"]]
+                try:
+                    del self.db[doc["_id"]]
+                    log.debug("Deleted ID: %s" % doc["_id"])
+                except:
+                    pass
                 try:
                     del self.db["{0}-distributable".format(doc["_id"])]
+                    log.debug("Deleted ID: %s" % "{0}-distributable".format(doc["_id"]))
                 except:
                     pass
         else:
             log.error("Not deleting test data!!!")
+        log.info ("Tearing Down Test Complete")
                 
+                
+    def _is_test_data(self, identifier):
+        try:
+            if re.search("^NSDL-TEST-DATA-", identifier) != None and re.search("-distributable$",identifier) == None and nsdl_data["ids"].index(identifier) >= 0:
+                return True
+            elif re.search("^OAI-DC-DATA-", identifier) != None and re.search("-distributable$",identifier) == None and nsdl_data["ids"].index(identifier) >= 0:
+                return True
+            else:
+                return False
+        except:
+            return False
+        
     def _get_timestamps(self, doc1, doc2):
         if doc1["node_timestamp"] < doc2["node_timestamp"]:
             from_ =  doc1["node_timestamp"]
@@ -131,32 +182,37 @@ class TestOaiPmhController(TestController):
         
         return (from_, until_)
     
-    def validate_lr_oai_response(self, response, errorExists=False, checkSchema=False, errorCodeExpected=None):
-        if hasattr(response, "lxml"):
-            xmlcontent = response.lxml
-        else:
-            body = response.body
-            xmlcontent = etree.fromstring(body)
+    def parse_response(self, response):
+        body = response.body
+        xmlcontent = etree.fromstring(body)
         
-        
+        return { "raw": body, "etree": xmlcontent }
+    
+    def validate_lr_oai_etree(self, xmlcontent, errorExists=False, checkSchema=False, errorCodeExpected=None):
         
         error = xmlcontent.xpath("//*[local-name()='error']", namespaces=namespaces)
         if errorExists == False:
             if len(error) > 0:
-                self.assertEqual(0, len(error), "validate_lr_oai_response FAIL: Error code:{0} mesg:{1}".format(error[0].xpath("@code", namespaces=namespaces)[0], error[0].xpath("text()", namespaces=namespaces)[0]))
+                self.assertEqual(0, len(error), "validate_lr_oai_etree FAIL: Error code:{0} mesg:{1}".format(error[0].xpath("@code", namespaces=namespaces)[0], error[0].xpath("text()", namespaces=namespaces)[0]))
         elif errorExists and errorCodeExpected != None:
             codeReceived = error[0].xpath("@code", namespaces=namespaces)[0]
             if errorCodeExpected != codeReceived:
-                self.assertEqual(0, len(error), "validate_lr_oai_response FAIL: Expected:{2}, Got Error code:{0} mesg:{1}".format(error[0].xpath("@code", namespaces=namespaces)[0], error[0].xpath("text()", namespaces=namespaces)[0], errorCodeExpected))
+                self.assertEqual(0, len(error), "validate_lr_oai_etree FAIL: Expected:{2}, Got Error code:{0} mesg:{1}".format(error[0].xpath("@code", namespaces=namespaces)[0], error[0].xpath("text()", namespaces=namespaces)[0], errorCodeExpected))
         else:
-            self.assertEqual(1, len(error), "validate_lr_oai_response FAIL: Expected error, none found.")
+            self.assertEqual(1, len(error), "validate_lr_oai_etree FAIL: Expected error, none found.")
         
         
         if checkSchema == True:
             self.oailrschema.assertValid(xmlcontent)
         else:
-            log.info("validate_lr_oai_response: Not validating response against schema.")
+            log.info("validate_lr_oai_etree: Not validating response against schema.")
         
+    def validate_lr_oai_response(self, response, errorExists=False, checkSchema=False, errorCodeExpected=None):
+        obj = self.parse_response(response)
+        xmlcontent = obj["etree"]
+        self.validate_lr_oai_etree(xmlcontent, errorExists, checkSchema, errorCodeExpected)
+        
+
     def test_empty(self):
             pass
         
@@ -181,6 +237,56 @@ class TestOaiPmhController(TestController):
         response = self.app.post("/OAI-PMH", params={'verb': 'Identify'})
         self.validate_lr_oai_response(response)
         log.info("test_identify_post: pass")
+        
+    def test_identify_earliest_datestamp(self):
+        '''verify that the network node maintains a value for the earliest publication time for documents harvestable from the node (earliestDatestamp)'''
+        response = self.app.post("/OAI-PMH", params={'verb': 'Identify'})
+        
+        root = etree.fromstring(response.body)
+        earliestDatestamp = root.xpath('/lr:OAI-PMH/lr:Identify/lr:earliestDatestamp/text()', namespaces=namespaces)
+        
+        assert len(earliestDatestamp) == 1, "Identify: missing earliest datestamp"
+        
+        try:
+            earliest_datestamp = iso8601.parse_date(earliestDatestamp[0])
+        except ParseError:
+            self.fail("Identify: earliestDatestamp does not parse as iso8601")
+        
+        log.info("test_identify_earliest_datestamp: pass")
+        
+    def test_identify_timestamp_granularity(self):
+        '''verify that the granularity of the timestamp exists in Identify.'''
+
+        response = self.app.post("/OAI-PMH", params={'verb': 'Identify'})
+        
+        root = etree.fromstring(response.body)
+        identifyGranularity = root.xpath('/lr:OAI-PMH/lr:Identify/lr:granularity/text()', namespaces=namespaces)
+        
+        assert len(identifyGranularity) == 1, "Identify: missing <granularity>"
+        
+        log.error("test_identify_timestamp_granularity: pass")
+        
+
+    def test_identify_timestamp_granularity_service_doc(self):
+        '''verify that the granularity of the timestamp is stored in the service description document.'''
+        serviceDoc = helpers.getServiceDocument(config["lr.oaipmh.docid"])
+        assert serviceDoc is not None, "Service document '%s' is missing." % config["lr.oaipmh.docid"]
+        
+        try:
+            granularity = serviceDoc["service_data"]["granularity"]
+        except:
+            self.fail("%s: granularity setting missing from service document." % config["lr.oaipmh.docid"])
+            
+        response = self.app.post("/OAI-PMH", params={'verb': 'Identify'})
+        
+        root = etree.fromstring(response.body)
+        identifyGranularity = root.xpath('/lr:OAI-PMH/lr:Identify/lr:granularity/text()', namespaces=namespaces)
+        
+        assert len(identifyGranularity) == 1, "Identify: missing <granularity>"
+        
+        assert identifyGranularity[0] == granularity, "Identify: <granularity> does not match service document setting."
+        
+        log.info("test_identify_timestamp_granularity_service_doc: pass")
         
         
     def test_ListSets_get(self):
@@ -242,6 +348,70 @@ class TestOaiPmhController(TestController):
         
 
 
+    def test_getRecord_by_doc_ID_match_requested_dissemination_get(self):
+        '''verify that the returned resource data matches the requested dissemination 
+        format for the specified  resource data description document ID or resource ID'''
+        
+        '''verify that if the request ID is a resource data description document 
+        ID, the service returns the metadata dissemination for the single resource 
+        data description document that matches the ID'''
+        
+        '''verify that the return of any resource_data that matches the requested 
+        dissemination format associated with the requested resource data document 
+        (any payload where the payload_schema matches the requested dissemination 
+        format) is supported.'''
+        
+        global nsdl_data, dc_data
+        randomDoc = choice(dc_data["documents"])
+        response = self.app.get("/OAI-PMH", params={'verb': 'GetRecord', 'metadataPrefix':'oai_dc', 'identifier': randomDoc["doc_ID"], 'by_doc_ID': True})
+        try:
+            obj = self.parse_response(response)           
+            
+            assert len(randomDoc["payload_schema"]) > 0, "test_getRecord_match_requested_dissemination_get: Test document missing payload_schema"
+            
+            hasSchema = False
+            for schema in randomDoc["payload_schema"]: 
+                if schema == "oai_dc":
+                    hasSchema = True
+                    continue
+            
+            assert hasSchema, "test_getRecord_match_requested_dissemination_get: Test document does not have a matching payload_schema"
+            
+            identifier = obj["etree"].xpath("/lr:OAI-PMH/lr:GetRecord/lr:record/lr:header/lr:identifier/text()", namespaces=namespaces)
+            
+            assert len(identifier) == 1 and identifier[0] == randomDoc["doc_ID"], "test_getRecord_match_requested_dissemination_get: Requested document does not match."
+            
+        except Exception as e:
+#            log.error("test_getRecord_by_doc_ID_get: fail - identifier: {0}".format(randomDoc["doc_ID"]))
+            log.exception("test_getRecord_match_requested_dissemination_get: fail - identifier: {0}".format(randomDoc["doc_ID"]))
+            global test_data_delete
+            test_data_delete = False
+            raise e
+        log.info("test_getRecord_match_requested_dissemination_get: pass")
+
+    
+    def test_getRecord_by_doc_ID_JSON_metadataPrefix_get(self):
+        '''verify that if the requested dissemination format in metadataPrefix 
+        matches the JSON metadataPrefix in the servcie description the service 
+        behaves as the basic harvest service (returns the complete resource data 
+        description document as JSON).'''
+        global nsdl_data, dc_data
+        randomDoc = choice(dc_data["documents"])
+        response = self.app.get("/OAI-PMH", params={'verb': 'GetRecord', 'metadataPrefix':'LR_JSON_0.10.0', 'identifier': randomDoc["doc_ID"], 'by_doc_ID': True})
+        try:
+            json_obj = json.load(response)
+            
+            assert len(json_obj["getrecord"]["record"]) == 1, "test_getRecord_by_doc_ID_JSON_metadataPrefix_get: No JSON record returned"
+            assert json_obj["getrecord"]["record"][0] == radomDoc, "test_getRecord_by_doc_ID_JSON_metadataPrefix_get: Returned document does not match."
+        except Exception as e:
+#            log.error("test_getRecord_by_doc_ID_get: fail - identifier: {0}".format(randomDoc["doc_ID"]))
+            log.exception("test_getRecord_by_doc_ID_JSON_metadataPrefix_get: fail - identifier: {0}".format(randomDoc["doc_ID"]))
+            global test_data_delete
+            test_data_delete = False
+            raise e
+        log.info("test_getRecord_by_doc_ID_JSON_metadataPrefix_get: pass")
+        
+        
     def test_getRecord_by_doc_ID_get(self):
         global nsdl_data, dc_data
         randomDoc = choice(dc_data["documents"])
@@ -271,39 +441,35 @@ class TestOaiPmhController(TestController):
         log.info("test_getRecord_by_doc_ID_post: pass")
         
     def test_getRecord_by_resource_ID_get(self):
-        global nsdl_data, dc_data
+        global nsdl_data, dc_data, test_data_delete
         randomDoc = choice(dc_data["documents"])
         response = self.app.get("/OAI-PMH", params={'verb': 'GetRecord', 'metadataPrefix':'oai_dc', 'identifier': randomDoc["resource_locator"], 'by_resource_ID': True})
         try:
             self.validate_lr_oai_response(response)
         except AssertionError:
-            global test_data_delete
             log.exception("test_getRecord_by_resource_ID_get: fail - identifier: {0}".format(randomDoc["resource_locator"]))
             test_data_delete = False
             raise
         except Exception as e:
 #            log.error("test_getRecord_by_resource_ID_get: fail - identifier: {0}".format(randomDoc["resource_locator"]))
             log.exception("test_getRecord_by_resource_ID_get: fail - identifier: {0}".format(randomDoc["resource_locator"]))
-            global test_data_delete
             test_data_delete = False
             raise e
         log.info("test_getRecord_by_resource_ID_get: pass")
         
     def test_getRecord_by_resource_ID_post(self):
-        global nsdl_data, dc_data
+        global nsdl_data, dc_data, test_data_delete
         randomDoc = choice(dc_data["documents"])
         response = self.app.post("/OAI-PMH", params={'verb': 'GetRecord', 'metadataPrefix':'oai_dc', 'identifier': randomDoc["resource_locator"], 'by_resource_ID': True})
         try:
             self.validate_lr_oai_response(response)
         except AssertionError:
-            global test_data_delete
             log.exception("test_getRecord_by_resource_ID_post: fail - identifier: {0}".format(randomDoc["resource_locator"]))
             test_data_delete = False
             raise
         except Exception as e:
 #            log.error("test_getRecord_by_resource_ID_post: fail - identifier: {0}".format(randomDoc["resource_locator"]))
             log.exception("test_getRecord_by_resource_ID_post: fail - identifier: {0}".format(randomDoc["resource_locator"]))
-            global test_data_delete
             test_data_delete = False
             raise e
         log.info("test_getRecord_by_resource_ID_post: pass")
@@ -327,6 +493,37 @@ class TestOaiPmhController(TestController):
             raise e
         log.info("test_listRecords_post: pass")
         
+    def test_listRecords_match_requested_disseminaton_get(self):
+        global nsdl_data, dc_data
+        doc1 = choice(nsdl_data["documents"])
+        doc2 = choice(nsdl_data["documents"])
+        
+        (from_, until_) = self._get_timestamps(doc1, doc2)
+            
+        response = self.app.get("/OAI-PMH", params={'verb': 'ListRecords', 'metadataPrefix': 'nsdl_dc', 'from': from_, 'until': until_})
+        try:
+            obj = self.parse_response(response)
+            oaipmh_root = obj["etree"]
+            
+            response_ids = oaipmh_root.xpath("./lr:ListRecords/lr:record/lr:header/lr:identifier/text()", namespaces=namespaces)
+            assert len(response_ids) > 0, "test_listRecords_match_requested_disseminaton_get: Unable to locate identifiers in response"
+            
+            for identifier in response_ids:
+                '''need to weed out any non-known test data since this is based upon a range'''
+                if self._is_test_data(identifier):
+                    try: 
+                        assert nsdl_data["ids"].index(identifier) >= 0, "Unexpected test identifier returned in result"
+                    except:
+                        self.fail("Identifier %s should have existed within result and did not" % identifier)
+            
+        except Exception as e:
+#            log.error("test_listRecords_get: fail - from: {0} until: {1}".format(from_, until_))
+            log.exception("test_listRecords_match_requested_disseminaton_get: fail - from: {0} until: {1}".format(from_, until_))
+            global test_data_delete
+            test_data_delete = False
+            raise e
+        log.info("test_listRecords_match_requested_disseminaton_get: pass")
+
     def test_listRecords_get(self):
         global nsdl_data, dc_data
         doc1 = choice(nsdl_data["documents"])
@@ -344,6 +541,7 @@ class TestOaiPmhController(TestController):
             test_data_delete = False
             raise e
         log.info("test_listRecords_get: pass")
+
         
         
     def test_listIdentifiers_post(self):
