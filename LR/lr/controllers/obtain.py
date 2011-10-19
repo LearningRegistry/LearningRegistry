c9b0faa8b5f5e2c4ae4ac9984739e291d07b55af
Walt Grata <wegrata@gmail.com>
2011-10-19 18:11:39 -0400

    Fixed capitalization of request_ID

--- LR/lr/controllers/obtain.py
+++ LR/lr/controllers/obtain.py
@@ -197,8 +197,8 @@ class ObtainController(BaseController):
             log.debug(data['resumption_token'])
         if request.params.has_key('callback'):
             data['callback'] = request.params['callback']
-        if request.params.has_key('request_id'):
-            data['request_IDs'].append(request.params['request_id'])
+        if request.params.has_key('request_ID'):
+            data['request_IDs'].append(request.params['request_ID'])
         return data        
     def edit(self, id, format='html'):
         """GET /obtain/id/edit: Form to edit an existing item"""
