8a41559dde34a0f8f17f94b84a7f02124a41de1c
John Poyau <john.poyau@lmco.com>
2011-11-15 14:04:52 -0500

    Fix some minor typos

--- config/services/Basic_Publish.py
+++ config/services/Basic_Publish.py
@@ -20,7 +20,7 @@ def install(server, dbname, setupInfo):
     custom_opts["doc_limit"] = int(active)
 
     active = getInput("Enter message size limit in octet. \n"+
-                    "This should the maximum publish data the the node  will accepts ", None, isInt)
+                    "This should the maximum data size  the the node  will accept ", None, isInt)
     custom_opts["msg_size_limit"] = int(active)
 
     custom_opts["node_endpoint"] = setupInfo["nodeUrl"]
