5e760fa8336d4acf3f60d8752c4d40143def995c
John Poyau <john.poyau@lmco.com>
2011-11-29 19:31:21 -0500

    Fix issue 120
    clean up and refactor the publish code. Move the code that handles the
    publishing to  controller/publish.py from model/__init__.py.
    
    Set the timestamp on all published documents.

--- LR/lr/controllers/publish.py
+++ LR/lr/controllers/publish.py
@@ -12,67 +12,133 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-import logging, urllib2, couchdb, json
-
+import logging
+import urllib2
+import  couchdb
+import  json
+import re
+import pprint
 from pylons import request, response, session, tmpl_context as c, url, config
 from pylons.controllers.util import abort, redirect
-
 from lr.lib.base import BaseController, render
-import lr.model as m
-import lr.lib.helpers as  h
-
+from  lr.model import ResourceDataModel, LRNode
+from  lr.lib import ModelParser, SpecValidationException, helpers as  h
 log = logging.getLogger(__name__)
+
 
 class PublishController(BaseController):
     """REST Controller styled on the Atom Publishing Protocol"""
     # To properly map this controller, ensure your config/routing.py
     # file has a resource setup:
     #     map.resource('publish', 'publisher')
-
-    def index(self, format='json'):
-        """GET /publisher: All items in the collection"""
-        # url('publisher')
-
+    __ERROR = 'error'
+    __OK = "OK"
+    __DOCUMENT_RESULTS =  'documents_results'
+    __DOCUMENTS = 'documents'
+    
     def create(self):
-        data = json.loads(request.body)
-        doc_limit =  h.getServiceDocument(config['lr.publish.docid'])['service_data']['doc_limit']
-        num_docs = len(data['documents'])
-        if num_docs < doc_limit:
-            results = map(m.publish,data['documents'])
-            return json.dumps({'OK':True, 'document_results':results})
+
+        results = {self.__OK:True}
+        error_message = None
+        try:
+            data = json.loads(request.body)
+            doc_limit =  h.getServiceDocument(config['lr.publish.docid'])['service_data']['doc_limit']
+            
+            if len(data[self.__DOCUMENTS]) > doc_limit:
+                error_message = "number of posted docs {0} exceeds doc limit: {1}".format(
+                                               len(data['documents']), str(doc_limit))
+                log.debug(error_message)
+                results[self.__ERROR] = error_message
+            else:
+                results[self.__DOCUMENT_RESULTS ] = map(self._publish, data[self.__DOCUMENTS])
+        except Exception as ex:
+            log.exception(ex)
+            results[self.__ERROR] = "internal error"
+        
+        if results.has_key(self.__ERROR):
+           results[self.__OK] = False
+        return json.dumps(results)
+        
+        
+    def _isResourceDataFilteredOut(self, resourceData):
+        
+        if (LRNode.filterDescription is None or
+            LRNode.filterDescription.filter is None or 
+            LRNode.filterDescription.custom_filter == True):
+            #Do custom the filter I supposed ... for now just resturn false.
+            return [False, None]
+        
+        matchResult = False
+        envelopFilter = ""
+        for f in LRNode.filterDescription.filter:
+            log.info("\n"+str(f)+"\n")
+            # Ckeck if jsonObject object has the key if it has search 
+            # for the regular expression in the filter otherwise keep looking
+            key = f['filter_key']
+            resourceValue = None
+            
+            for k in resourceData._specData.keys():
+                if re.search(key, k) is not None:
+                    resourceValue = str(resourceData.__getattr__(k))
+                    break
+                    
+            if  resourceValue is None:
+                continue
+                
+            value = f['filter_value']
+            log.info("\n"+str(key)+", "+str(value)+", "+ resourceValue+"\n")
+            if(re.search(value,  resourceValue) is not None):
+                matchResult = True
+                envelopFilter = "Filter '"+str(f)+"' for key '"+str(key)+\
+                                          "' matches '"+resourceValue+"'"
+                break
+        
+        #Check if what matching means base on the include_exclude
+        # True: the filters describe what documents to accept all others
+        # are rejected
+        # False: the filters describe what documents to reject
+        # all others are accepted
+        if LRNode.filterDescription.include_exclude is None or \
+            LRNode.filterDescription.include_exclude == True:
+            if matchResult == False:
+                return True, "\nDocument failed to match filter: \n"+\
+                               pprint.pformat(LRNode.filterDescription.specData, indent=4)+"\n"
         else:
-             return json.dumps({'OK':False, 
-                                        'error':"number of posted docs {0} exceeds doc limit: {1}".format(num_docs, str(doc_limit))})
-        """POST /publisher: Create a new item"""
+            if matchResult == True:
+               return True, "\nExcluded by filter: \n"+envelopFilter
+             
+        return [False, None]
+        
+    def _publish(self, envelopData):
+        if isinstance(envelopData,unicode):
+            envelopeData = json.loads(envelopData)
+            
+        result={self.__OK: True}
 
-    def new(self, format='html'):
-        return 'new'
-        """GET /publisher/new: Form to create a new item"""
-        # url('new_publish')
-
-    def update(self, id):
-        """PUT /publisher/id: Update an existing item"""
-        # Forms posted to this method should contain a hidden field:
-        #    <input type="hidden" name="_method" value="PUT" />
-        # Or using helpers:
-        #    h.form(url('publish', id=ID),
-        #           method='put')
-        # url('publish', id=ID)
-
-    def delete(self, id):
-        return 'delete'
-        """DELETE /publisher/id: Delete an existing item"""
-        # Forms posted to this method should contain a hidden field:
-        #    <input type="hidden" name="_method" value="DELETE" />
-        # Or using helpers:
-        #    h.form(url('publish', id=ID),
-        #           method='delete')
-        # url('publish', id=ID)
-
-    def show(self, id, format='json'):
-        """GET /publisher/id: Show a specific item"""
-        # url('publish', id=ID)
-    def edit(self, id, format='html'):
-        return 'edit'
-        """GET /publisher/id/edit: Form to edit an existing item"""
-        # url('edit_publish', id=ID)
+        try:
+            # Set the envelop data timestaps.
+            timeStamp = h. nowToISO8601Zformat()
+            for stamp in ResourceDataModel._TIME_STAMPS:
+                    envelopData[stamp] = timeStamp
+                    
+            resourceData = ResourceDataModel(envelopData)
+            #Check if the envelop get filtered out
+            isFilteredOut, reason = self._isResourceDataFilteredOut(resourceData)
+            if isFilteredOut:
+                result[self.__ERROR] = reason
+            else:
+                resourceData.publishing_node = LRNode.nodeDescription.node_id
+                resourceData.save()
+                result[resourceData._DOC_ID] = resourceData.doc_ID 
+                 
+        except SpecValidationException as ex:
+            log.exception(ex)
+            result[self.__ERROR]  = "\n"+pprint.pformat(str(ex), indent=4)
+        except Exception as ex:
+            log.exception(ex)
+            result[self.__ERROR]  = "internal error"
+            
+        if result.has_key(self.__ERROR):
+            result[self.__OK] = False
+        
+        return result
