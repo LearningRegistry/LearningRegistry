ada55d8d66a39c2ada47b20945b41321d867f2c0
Walt Grata <wegrata@gmail.com>
2011-12-08 12:46:35 -0500

    Removed bad tests that were commented out

--- LR/lr/tests/functional/test_description.py
+++ LR/lr/tests/functional/test_description.py
@@ -3,42 +3,4 @@ from lr.tests import *
 class TestDescriptionController(TestController):
 
     def test_index(self):
-        pass
-        #response = self.app.get(url('description'))
-        # Test response...
-
-    # def test_index_as_xml(self):
-    #     response = self.app.get(url('formatted_description', format='xml'))
-
-    # def test_create(self):
-    #     response = self.app.post(url('description'))
-
-    # def test_new(self):
-    #     response = self.app.get(url('new_description'))
-
-    # def test_new_as_xml(self):
-    #     response = self.app.get(url('formatted_new_description', format='xml'))
-
-    # def test_update(self):
-    #     response = self.app.put(url('description', id=1))
-
-    # def test_update_browser_fakeout(self):
-    #     response = self.app.post(url('description', id=1), params=dict(_method='put'))
-
-    # def test_delete(self):
-    #     response = self.app.delete(url('description', id=1))
-
-    # def test_delete_browser_fakeout(self):
-    #     response = self.app.post(url('description', id=1), params=dict(_method='delete'))
-
-    # def test_show(self):
-    #     response = self.app.get(url('description', id=1))
-
-    # def test_show_as_xml(self):
-    #     response = self.app.get(url('formatted_description', id=1, format='xml'))
-
-    # def test_edit(self):
-    #     response = self.app.get(url('edit_description', id=1))
-
-    # def test_edit_as_xml(self):
-    #     response = self.app.get(url('formatted_edit_description', id=1, format='xml'))
+        pass    
