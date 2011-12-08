fc172c35c32106c3b2e2cfcd49bb998734d94e4f
Walt Grata <wegrata@gmail.com>
2011-12-08 10:47:31 -0500

    commented code out rather than converting to string

--- LR/lr/tests/functional/test_contrib_filters.py
+++ LR/lr/tests/functional/test_contrib_filters.py
@@ -7,38 +7,38 @@ class TestFiltersController(TestController):
         #response = self.app.get(url('contrib_filters'))
         # Test response...
 
-    '''def test_index_as_xml(self):
-        response = self.app.get(url('formatted_contrib_filters', format='xml'))
+    # def test_index_as_xml(self):
+    #     response = self.app.get(url('formatted_contrib_filters', format='xml'))
 
-    def test_create(self):
-        response = self.app.post(url('contrib_filters'))
+    # def test_create(self):
+    #     response = self.app.post(url('contrib_filters'))
 
-    def test_new(self):
-        response = self.app.get(url('contrib_new_filter'))
+    # def test_new(self):
+    #     response = self.app.get(url('contrib_new_filter'))
 
-    def test_new_as_xml(self):
-        response = self.app.get(url('formatted_contrib_new_filter', format='xml'))
+    # def test_new_as_xml(self):
+    #     response = self.app.get(url('formatted_contrib_new_filter', format='xml'))
 
-    def test_update(self):
-        response = self.app.put(url('contrib_filter', id=1))
+    # def test_update(self):
+    #     response = self.app.put(url('contrib_filter', id=1))
 
-    def test_update_browser_fakeout(self):
-        response = self.app.post(url('contrib_filter', id=1), params=dict(_method='put'))
+    # def test_update_browser_fakeout(self):
+    #     response = self.app.post(url('contrib_filter', id=1), params=dict(_method='put'))
 
-    def test_delete(self):
-        response = self.app.delete(url('contrib_filter', id=1))
+    # def test_delete(self):
+    #     response = self.app.delete(url('contrib_filter', id=1))
 
-    def test_delete_browser_fakeout(self):
-        response = self.app.post(url('contrib_filter', id=1), params=dict(_method='delete'))
+    # def test_delete_browser_fakeout(self):
+    #     response = self.app.post(url('contrib_filter', id=1), params=dict(_method='delete'))
 
-    def test_show(self):
-        response = self.app.get(url('contrib_filter', id=1))
+    # def test_show(self):
+    #     response = self.app.get(url('contrib_filter', id=1))
 
-    def test_show_as_xml(self):
-        response = self.app.get(url('formatted_contrib_filter', id=1, format='xml'))
+    # def test_show_as_xml(self):
+    #     response = self.app.get(url('formatted_contrib_filter', id=1, format='xml'))
 
-    def test_edit(self):
-        response = self.app.get(url('contrib_edit_filter', id=1))
+    # def test_edit(self):
+    #     response = self.app.get(url('contrib_edit_filter', id=1))
 
-    def test_edit_as_xml(self):
-        response = self.app.get(url('formatted_contrib_edit_filter', id=1, format='xml'))'''
+    # def test_edit_as_xml(self):
+    #     response = self.app.get(url('formatted_contrib_edit_filter', id=1, format='xml'))
