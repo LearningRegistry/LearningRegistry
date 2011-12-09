5952574df2b3edf9fb83b55ae48114e5c9e70e46
Walt Grata <wegrata@gmail.com>
2011-12-09 09:22:49 -0500

    removed username, password params.  They were redundent with -u and -p params to fabric.  Also refactored the commands to make them easier to compose

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -3,26 +3,44 @@ def dev():
     env.hosts = ['lrdev01.learningregistry.org',
                  'lrdev02.learningregistry.org']    
 
-def test():
-    env.hosts = ['lrtest01.learningregistry.org',
-                 'lrtest02.learningregistry.org']
+def sandbox():
+    env.hosts = ['sandbox.learningregistry.org']
+
+def alpha():
+    env.hosts = ['alpha.learningregistry.org']
+    
 def prod():    
     env.hosts = ['node01.public.learningregistry.net',
                  'node03.public.learningregistry.net',
                  'node03.public.learningregistry.net']
 
-def deploy(username, password, branch, runTests=False):
-    env.user = username
-    env.password = password
-    run('cd ~/gitrepos/LearningRegistry;git checkout master;git pull;git checkout origin/%s;' % branch)
-    run('. ~/virtualenv/lr/bin/activate;cd ~/gitrepos/LearningRegistry/LR;pip uninstall LR;yes | pip -q install .;yes | pip install -q uwsgi;')
+def deploy(branch, runTests=False):
+    activateLrVirtualEnv = '. ~/virtualenv/lr/bin/activate'
+    changeToLRProjectDir = 'cd ~/gitrepos/LearningRegistry/LR'
+    deactivate = 'deactivate'
+    gitCheckout = ['cd ~/gitrepos/LearningRegistry',
+                   'git checkout master',
+                   'git pull',
+                   'git checkout origin/%s' % branch]
+    updateLRInstall = [activateLrVirtualEnv,
+                       changeToLRProjectDir,
+                       'pip uninstall LR',
+                       'yes | pip -q install .',
+                       'yes | pip install -q uwsgi',
+                       deactivate]
+    startLRServer = [activateLrVirtualEnv,
+                     changeToLRProjectDir,
+                     'uwsgi --ini-paste development.ini --virtualenv ~/virtualenv/lr/ --daemonize uwsgi.log --pidfile uwsgi.pid',
+                     deactivate]
+    testCommand = ['. ~/virtualenv/lr/bin/activate',
+                   'cd ~/gitrepos/LearningRegistry/LR',
+                   'nosetests lr/tests/functional/test_harvest.py',
+                   'nosetests lr/tests/functional/test_obtain.py',
+                   'nosetests lr/tests/functional/test_OAI-OMH.py',
+                   'nosetests lr/tests/functional/test_slice.py']                     
+    run(';'.join(gitCheckout))
+    run(';'.join(updateLRInstall))
     run('killall -9 uwsgi;')
     if runTests:
-        testCommand = '. ~/virtualenv/lr/bin/activate;'
-        testCommand += 'cd ~/gitrepos/LearningRegistry/LR;'
-        testCommand += 'nosetests lr/tests/functional/test_harvest.py;'
-        testCommand += 'nosetests lr/tests/functional/test_obtain.py;'
-        testCommand += 'nosetests lr/tests/functional/test_OAI-OMH.py;'
-        testCommand += 'nosetests lr/tests/functional/test_slice.py;'
-        run(testCommand)
-    run('. ~/virtualenv/lr/bin/activate;cd ~/gitrepos/LearningRegistry/LR/;uwsgi --ini-paste development.ini --virtualenv ~/virtualenv/lr/ --daemonize uwsgi.log --pidfile uwsgi.pid;deactivate;')
+        run(';'.join(testCommand))
+    run(';'.join(startLRServer))
