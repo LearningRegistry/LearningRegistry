8808861caf1d2f4b3ca130d80d255aeda2d695e6
Walt Grata <wegrata@gmail.com>
2012-03-08 10:23:11 -0500

    Updated data services prototype

--- LR/lr/config/routing.py
+++ LR/lr/config/routing.py
@@ -55,7 +55,8 @@ def make_map(config):
         mapResource('lr.oaipmh.docid', 'OAI-PMH', 'OAI-PMH')
         mapResource('lr.slice.docid', 'slice', 'slice')
         mapResource('lr.sword.docid', 'swordservice','swordservice')    
-    map.connect("/extract/{view}/{param}",controller='extract', action='get')
+    map.connect("/extract/{dataservice}/{view}",controller='extract', action='get')
+    map.connect("/extract//{dataservice}/{view}/format/{show}",controller='extract', action='get')
     map.minimization = False
     map.explicit = False
 
