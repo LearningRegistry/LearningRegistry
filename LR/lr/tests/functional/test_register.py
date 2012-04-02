cb52a4dddb62a662f171df72fc710dd84b147453
Walt Grata <wegrata@gmail.com>
2012-04-02 11:58:15 -0400

    added basic unit tests

--- LR/lr/tests/functional/test_register.py
+++ LR/lr/tests/functional/test_register.py
@@ -4,4 +4,7 @@ class TestRegisterController(TestController):
 
     def test_index(self):
         response = self.app.get(url(controller='register', action='index'))
-        # Test response...
+        assert response.headers['Content-Type'] == 'text/html;charset=utf-8'
+    def test_create(self):
+    	response = self.app.post(url(controller="register",action="create"),params={"destUrl":"123"})
+    	assert response.status_int == 200
