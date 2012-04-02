839afbdf94dafdb5ffd695ece70c7bb57da23a92
Walt Grata <wegrata@gmail.com>
2012-04-02 12:33:16 -0400

    added view compaction

--- LR/lr/lib/couch_change_monitor/base_views_update_handler.py
+++ LR/lr/lib/couch_change_monitor/base_views_update_handler.py
@@ -24,7 +24,8 @@ class BaseViewsUpdateHandler(BaseChangeThresholdHandler):
     def _updateView(self, viewUrl):
         log.debug('start view update %s' % viewUrl)
         log.debug(urllib2.urlopen(viewUrl).read())
-        
+    def _compactView(self,compactUrl):
+        log.debug(urllib2.urlopen(compactUrl).read())
     def _handle (self, change, database):
         log.debug("class: {0} Updating views ...".format(self.__class__.__name__))
         try:
@@ -32,8 +33,12 @@ class BaseViewsUpdateHandler(BaseChangeThresholdHandler):
                                                         startkey='_design%2F',endkey='_design0')
             for designDoc in designDocs:
                 viewInfo = "{0}/{1}/_info".format(database.resource.url, designDoc.id)
-                viewInfo = json.load(urllib2.urlopen(viewInfo))
                 
+
+                viewInfo = json.load(urllib2.urlopen(viewInfo))
+                if (not viewInfo['view_index']['compact_running']):
+                    compactUrl = "{0}/_compact/{1}".format(database.resource.url, designDoc.id)
+                    multiprocessing.Process(target=self._compactView, args=(compactUrl,)).start()                        
                 if (not viewInfo['view_index']['updater_running'] and 
                     designDoc.doc.has_key('views') and len(designDoc.doc['views']) > 0):
 
