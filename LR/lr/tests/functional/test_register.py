fd43b841df601f479bac7cd5697b48088c0017a0
Walt Grata <wegrata@gmail.com>
2012-04-02 12:23:12 -0400

    updated tests and error responses

--- LR/lr/tests/functional/test_register.py
+++ LR/lr/tests/functional/test_register.py
@@ -6,5 +6,8 @@ class TestRegisterController(TestController):
         response = self.app.get(url(controller='register', action='index'))
         assert response.headers['Content-Type'] == 'text/html;charset=utf-8'
     def test_create(self):
-    	response = self.app.post(url(controller="register",action="create"),params={"destUrl":"123"})
+    	response = self.app.post(url(controller="register",action="create"),params={"destUrl":"http://localhost/resource_data"})
     	assert response.status_int == 200
+    def test_create_invalid_url(self):
+    	response = self.app.post(url(controller="register",action="create"),params={"destUrl":"123"}, status=400)
+    	assert response.status_int == 400
