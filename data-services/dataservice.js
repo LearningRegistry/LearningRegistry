218b3556a5c4473302e9690540dba2095a5a6709
Jim Klo <jim.klo@sri.com>
2012-04-06 19:36:27 -0700

    Added more documentation and examples

--- data-services/dataservice.js
+++ data-services/dataservice.js
@@ -9,4 +9,29 @@ $(function() {
     $('#transition-themes').change(function() {
         $('#transition-theme-link').attr('href', $(this).val());
     });
+
+    var server = "http://learnreg1.sri.com";
+    function runQuery( url ) {
+
+        var settings = {
+            dataType: 'text',
+            callback: 'callback',
+            success: function (data, textStatus, jqXhr) {
+                $('#extract-query-examples .loading').hide(500);
+                 $('#extract-query-examples .result').html("<code>"+JSON.stringify(JSON.parse(data), null, 2)+"</code>").show(500);
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                $('#extract-query-examples .loading').hide(500);
+                $('#extract-query-examples .result').html("<h4>"+textStatus+"</h4>").show();
+                $('#extract-query-examples .result').append(errorThrown);
+            }
+        }
+        $.ajax(server+url, settings);
+    }
+
+    $('#extract-query-examples button').click(function(){
+        $('#extract-query-examples .loading').show(500);
+        $('#extract-query-examples .result').hide(500);
+        runQuery($('#extract-query-examples textarea').val());
+    })
 });
