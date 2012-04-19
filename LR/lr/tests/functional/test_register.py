f4be1bf0f7818947c552f3160157fc3b6fbc719e
Jim Klo <jim.klo@sri.com>
2012-04-19 13:56:50 -0700

    change destination url to something 'almost' guaranteed.

--- LR/lr/tests/functional/test_register.py
+++ LR/lr/tests/functional/test_register.py
@@ -6,7 +6,7 @@ class TestRegisterController(TestController):
         response = self.app.get(url(controller='register', action='index'))
         assert response.headers['Content-Type'] == 'text/html;charset=utf-8'
     def test_create(self):
-    	response = self.app.post(url(controller="register",action="create"),params={"destUrl":"http://localhost/resource_data"})
+    	response = self.app.post(url(controller="register",action="create"),params={"destUrl":"http://localhost:5984/resource_data"})
     	assert response.status_int == 200
     def test_create_invalid_url(self):
     	response = self.app.post(url(controller="register",action="create"),params={"destUrl":"123"}, status=400)
