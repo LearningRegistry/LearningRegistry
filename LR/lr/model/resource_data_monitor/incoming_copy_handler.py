d15f4a05469dd31aa6f4c0872b1bfcdaa0b3b11f
Walt Grata <wegrata@gmail.com>
2012-04-25 15:22:44 -0400

    added except clause for conflicts to keep from getting logged twice

--- LR/lr/model/resource_data_monitor/incoming_copy_handler.py
+++ LR/lr/model/resource_data_monitor/incoming_copy_handler.py
@@ -10,6 +10,7 @@ from pylons import config
 from lr.lib import ModelParser, SpecValidationException,helpers as h
 from lr.lib.couch_change_monitor import BaseChangeHandler
 from lr.model import ResourceDataModel
+from couchdb import ResourceConflict
 log = logging.getLogger(__name__)
 
 scriptPath = os.path.dirname(os.path.abspath(__file__))
@@ -17,7 +18,8 @@ _PYLONS_CONFIG =  os.path.join(scriptPath, '..', '..', '..', 'development.ini.or
 _config = ConfigParser.ConfigParser()
 _config.read(_PYLONS_CONFIG)
 
-_RESOURCE_DISTRIBUTABLE_TYPE = "resource_data"
+_RESOURCE_DISTRIBUTABLE_TYPE = "resource_data_distributable"
+_RESOURCE_TYPE = "resource_data"
 _DOC_TYPE = "doc_type"
 _DOC = "doc"
 
@@ -31,7 +33,7 @@ class IncomingCopyHandler(BaseChangeHandler):
 		self._db = s[self._targetName]
 
 	def _canHandle(self, change, database):
-		if ((_DOC in change) and (change[_DOC].get(_DOC_TYPE) ==_RESOURCE_DISTRIBUTABLE_TYPE)):
+		if ((_DOC in change) and (change[_DOC].get(_DOC_TYPE) ==_RESOURCE_DISTRIBUTABLE_TYPE or change[_DOC].get(_DOC_TYPE) == _RESOURCE_TYPE)):
 			return True
 		return False
 
@@ -47,6 +49,7 @@ class IncomingCopyHandler(BaseChangeHandler):
 			del database[newDoc['node_ID']]
 		except SpecValidationException:
 			log.error(str(newDoc) + " Fails Validation" )
+		except ResourceConflict:
+			pass #ignore conflicts
 		except Exception as ex:
 			log.error(ex)
-			pass#ignore conflicts when copying
