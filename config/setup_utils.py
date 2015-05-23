a6b31c295bfb26907a3a7fd550e3994038cc25bd
Jim Klo <jim@arkitec.com>
2015-05-23 10:00:04 -0700

    invert logic

--- config/setup_utils.py
+++ config/setup_utils.py
@@ -311,7 +311,7 @@ def setNodeSigning(server, config, setupInfo):
         config.set("app:main","lr.publish.signing.signer",signer)
 
 
-        passphrase = getInput("Passphrase for Signing with Private Key [typing is concealed]", "", checkPassphrase(gpg, privateKeyId), hide_input=setupInfo["show_pass"])
+        passphrase = getInput("Passphrase for Signing with Private Key [typing is concealed]", "", checkPassphrase(gpg, privateKeyId), hide_input=!setupInfo["show_pass"])
         setupInfo["lr.publish.signing.passphrase"] = passphrase
         config.set("app:main","lr.publish.signing.passphrase",passphrase)
 
