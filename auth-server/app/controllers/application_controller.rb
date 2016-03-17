d5f26ecc97f5f60394a3a2d4730f8ed749baf1ff
Abraham Sánchez <abraham@twintags.com>
2016-03-17 11:29:29 +0100

    Stop using rails-api gem
    Since we're using regular views and cookie based
    sessions its use is not really as useful as
    expected.

--- auth-server/app/controllers/application_controller.rb
+++ auth-server/app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
-class ApplicationController < ActionController::API
+# Main application controller
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
 end
--- auth-server/config/application.rb
+++ auth-server/config/application.rb
@@ -25,9 +25,6 @@ module AuthServer
     # Do not swallow errors in after_commit/after_rollback callbacks.
     # config.active_record.raise_in_transactional_callbacks = true
 
-    # Needed to enable session store for omniauth
-    config.api_only = false
-
     # CORS configuration
     config.middleware.insert_before 0, 'Rack::Cors' do
       allow do
