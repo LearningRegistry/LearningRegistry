72d594c2c824c02388876da7868623ac0dc10441
Abraham Sánchez <abraham@twintags.com>
2016-03-26 12:04:01 +0100

    Include SMTP configuration for production env.

--- auth-server/config/environments/production.rb
+++ auth-server/config/environments/production.rb
@@ -76,4 +76,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # SMTP configuration
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings =
+    {
+      address: ENV['SMTP_ADDRESS'],
+      port: 587,
+      user_name: ENV['SMTP_USERNAME'],
+      password: ENV['SMTP_PASSWORD'],
+      authentication: :login,
+      enable_starttls_auto: true
+    }
 end
